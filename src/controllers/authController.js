6a055965ec016a074336d45aa82f8cb30614e499
devEddu17x <eduarjma@gmail.com>
2024-06-16 16:00:06 -0500

    update auth controller

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,25 +1,23 @@
-import { validate, validatePartial } from '../schemas/schemaValidator.js'
+import { validate } from '../schemas/schemaValidator.js'
 import { authSchema } from '../schemas/entities/authSchema.js'
 import { userSchema } from '../schemas/entities/userSchema.js'
-import { userDetailsSchema } from '../schemas/entities/userDetailsSchema.js'
+import { UserController } from './userController.js'
 
 export class AuthController {
-  constructor ({ userMovel }) {
-    this.userModel = userMovel
+  constructor ({ userModel }) {
+    this.userController = new UserController({ userModel })
   }
 
+  // data base has a trigger that create by itself userDetails record, so i just need to create an user
   async register (req, res) {
     const essentialData = validate(userSchema, req.body)
     if (!essentialData.succes) return res.send(essentialData.error)
-    // create user using user controller
-    const detailsData = validatePartial(userDetailsSchema, req.body)
-    if (!detailsData.succes) {
-      // create userDetails even detailsData was wrong, send only userId -> createdAt,followersCount, followerdCount,and others will be default in db
-    }
-    // create userDetails with data
+    return this.userController.create(essentialData.data, res)
   }
 
+  // login in building
   async login (req, res) {
+    // bulding login...
     const result = validate(authSchema, req.body)
     if (!result.succes) return res.send(result.error)
     // log user
