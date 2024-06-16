5f28500a82ca234c1a60d633157a917540d411a4
devEddu17x <eduarjma@gmail.com>
2024-06-16 15:59:03 -0500

    add userDetails model

---
+++ src/models/userDetails.js
@@ -0,0 +1,37 @@
+import { db } from '../config/db/mysql.js'
+export class UserModel {
+  async update ({ id, input }) {
+    const list = []
+    const values = []
+    for (const [key, value] of Object.entries(input)) {
+      list.push(`${key} = ?`)
+      values.push(value)
+    }
+    list.join(',')
+    values.push(id)
+    const query = `
+    UPDATE users
+    SET ${list}
+    WHERE id = UUID_TO_BIN(?)
+    `
+    try {
+      const [result] = await db.execute(query, values)
+      return result.affectedRows > 0 ? { succes: true, message: 'User updated' } : { error: 'User not updated' }
+    } catch (e) {
+      return ({ error: 'Error updating user' })
+    }
+  }
+
+  async getById ({ id }) {
+    try {
+      const [users] = await db.execute(`
+        SELECT username, email, name, lastname
+        FROM users
+        WHERE id = UUID_TO_BIN(?)
+        `, [id])
+      return users.length !== 0 ? { succes: true, data: users[0] } : { error: 'User not found' }
+    } catch (e) {
+      return { error: 'Can not find user' }
+    }
+  }
+}
