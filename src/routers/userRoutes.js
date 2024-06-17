3e6b15668ea105546b4b0223e7d433cc4c7a2b8e
devEddu17x <eduarjma@gmail.com>
2024-06-17 18:49:34 -0500

    feat using middleware to validate id in users route

--- src/routers/userRoutes.js
+++ src/routers/userRoutes.js
@@ -1,11 +1,12 @@
 import { Router } from 'express'
 import { UserController } from '../controllers/userController.js'
+import { validateIdExist } from '../middlewares/validateUUIDExists.js'
 export const createUserRouter = ({ userModel }) => {
   const userRouter = Router()
   const userController = new UserController({ userModel })
   userRouter.get('/', userController.getByQuery) // get an user by name or lastname or both
-  userRouter.get('/:id', userController.getById) // get an user by id
-  userRouter.delete('/:id', userController.delete) // delete an user by id
+  userRouter.get('/:id', validateIdExist, userController.getById) // get an user by id
+  userRouter.delete('/:id', validateIdExist, userController.delete) // delete an user by id
   return userRouter
 }
 
