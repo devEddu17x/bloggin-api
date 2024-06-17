33b87da5b3a984a2021962ea3f3833669ce593c0
devEddu17x <eduarjma@gmail.com>
2024-06-16 20:18:37 -0500

    add support for searching by query and change key for  positive response in response

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -7,27 +7,34 @@ export class UserController {
   create = async (req, res) => {
     const id = randomUUID()
     const result = await this.userModel.create({ id, input: req })
-    res.send((!result.success) ? { error: result.error } : { success: result.message })
+    res.send((!result.success) ? { error: result.error } : { user: result.message })
   }
 
   getById = async (req, res) => {
     const { id } = req.params
     if (!id) return { invalid_id: 'Valid id as UUUID is required' }
-    const result = this.userModel.getById({ id })
-    res.send((!result.success) ? { error: result.error } : { success: result.message })
+    const result = await this.userModel.getById({ id })
+    res.send((!result.success) ? { error: result.error } : { user: result.data })
+  }
+
+  getByQuery = async (req, res) => {
+    const { name, lastname } = req.query
+    if (!name && !lastname) res.status(400).send({ error: 'Name or lastname are required at this endpoint' })
+    const result = await this.userModel.getByQuery({ query: req.query })
+    res.send((!result.success) ? { error: result.error } : { user: result.data })
   }
 
   update = async (req, res) => {
     const { id } = req.params
     if (!id) return { invalid_id: 'Valid id as UUUID is required' }
     const result = this.userModel.update({ id, input: req.body })
-    res.send((!result.success) ? { error: result.error } : { success: result.message })
+    res.send((!result.success) ? { error: result.error } : { newUser: result.message })
   }
 
   delete = async (req, res) => {
     const { id } = req.params
     if (!id) return { invalid_id: 'Valid id as UUUID is required' }
     const result = this.userModel.delete({ id })
-    res.send((!result.success) ? { error: result.error } : { success: result.message })
+    res.send((!result.success) ? { error: result.error } : { message: result.message })
   }
 }
