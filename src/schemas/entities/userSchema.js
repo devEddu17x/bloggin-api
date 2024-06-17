3636af111c276905c13e1a93e7b42ae078bd5fc1
devEddu17x <eduarjma@gmail.com>
2024-06-17 16:42:57 -0500

    update user schema... again

--- src/schemas/entities/userSchema.js
+++ src/schemas/entities/userSchema.js
@@ -3,7 +3,7 @@ import { ERRORS } from '../messages/error.js'
 
 export const userSchema = z.object({
   username: z.string(ERRORS.REQUIRED('Username'))
-    .min(5, ERRORS.MIN('Username', 5))
+    .min(3, ERRORS.MIN('Username', 3))
     .max(50, ERRORS.MAX('Username', 50)),
 
   email: z.string(ERRORS.REQUIRED('Email adress'))
@@ -15,10 +15,10 @@ export const userSchema = z.object({
     .max(60, ERRORS.MAX('Password', 60)),
 
   name: z.string(ERRORS.REQUIRED('Name'))
-    .min(5, ERRORS.MIN('Name', 5))
+    .min(3, ERRORS.MIN('Name', 3))
     .max(20, ERRORS.MAX('Name', 20)),
 
   lastName: z.string(ERRORS.REQUIRED('Lastname'))
-    .min(5, ERRORS.MIN('Lastname', 5))
+    .min(3, ERRORS.MIN('Lastname', 3))
     .max(20, ERRORS.MAX('Lastname', 20))
 })
