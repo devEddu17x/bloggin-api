245d2d7efdf42eb5c01e05adc4f9df7bab036479
devEddu17x <eduarjma@gmail.com>
2024-06-16 20:20:00 -0500

    update app to support users router

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 import express, { json } from 'express'
 import { createAuthRoute } from './routers/authRoutes.js'
-
+import { createUserRouter } from './routers/userRoutes.js'
 export const createApp = ({ userModel, postModel, commentModel }) => {
   const app = express()
   app.use(json())
   app.disable('x-powered-by')
   app.use('/auth', createAuthRoute({ userModel }))
+  app.use('/users', createUserRouter({ userModel }))
   const PORT = 3000
   app.listen(PORT, () => {
     console.log(`Server listening on port http://localhost:${PORT}`)
