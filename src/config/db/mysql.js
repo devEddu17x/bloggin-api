efe05828ca57668158f43c86b2fbe74e4d9d9c7b
devEddu17x <eduarjma@gmail.com>
2024-06-16 19:01:09 -0500

    fix db connection

--- src/config/db/mysql.js
+++ src/config/db/mysql.js
@@ -1,5 +1,5 @@
 import mysql from 'mysql2/promise'
-export const db = mysql.createConnection({
+export const db = await mysql.createConnection({
   host: process.env.HOST,
   user: process.env.USER,
   password: process.env.PASSWORD,
--- src/utils/db.js
+++
@@ -1,3 +0,0 @@
-import mysql from 'mysql2/promise'
-import { mysqConfig } from '../config/db/mysql.js'
-export const connection = await mysql.createConnection(mysqConfig)
