1bc7b8247a8ab551e57f14d3e78ad7836472f38f
devEddu17x <eduarjma@gmail.com>
2024-06-17 22:16:49 -0500

    update auth schema add email to schema

--- src/schemas/entities/authSchema.js
+++ src/schemas/entities/authSchema.js
@@ -1,8 +1,11 @@
 import { z } from 'zod'
 import { ERRORS } from '../messages/error.js'
 export const authSchema = z.object({
+  email: z.string(ERRORS.REQUIRED('Email adress'))
+    .email(ERRORS.INVALID('Email adress'))
+    .max(50, ERRORS.MAX('Email adress', 50)),
   username: z.string(ERRORS.REQUIRED('Username'))
-    .min(5, ERRORS.MIN('Username', 5))
+    .min(3, ERRORS.MIN('Username', 3))
     .max(50, ERRORS.MAX('Username', 50)),
   password: z.string(ERRORS.REQUIRED('Password'))
     .min(8, ERRORS.MIN('Password', 8))
