e3abcf771d3cc0051673f6487ce6aec47f70aae5
devEddu17x <eduarjma@gmail.com>
2024-06-17 22:25:33 -0500

    feat implement middleware for login in auth router

--- src/routers/authRoutes.js
+++ src/routers/authRoutes.js
@@ -1,9 +1,10 @@
 import { Router } from 'express'
 import { AuthController } from '../controllers/authController.js'
+import { validateUsernameOrEmail } from '../middlewares/loginMiddleware.js'
 export const createAuthRoute = ({ userModel }) => {
   const authRouter = Router()
   const authController = new AuthController({ userModel })
   authRouter.post('/register', authController.register)
-  authRouter.post('/login', authController.login)
+  authRouter.post('/login', validateUsernameOrEmail, authController.login)
   return authRouter
 }
