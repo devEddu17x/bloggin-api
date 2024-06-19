5b4bb57dedff3f492311a69e87f30d0179c0b04b
devEddu17x <eduarjma@gmail.com>
2024-06-18 22:22:36 -0500

    refactor: uuid middlware

--- src/middlewares/validateUUIDExists.js
+++ src/middlewares/validateUUIDExists.js
@@ -1,8 +1,8 @@
 import { UUIDSchema } from '../schemas/entities/UUIDSchema.js'
 import { validate } from '../schemas/schemaValidator.js'
 export const validateIdExist = (req, res, next) => {
-  const { id } = req.params
-  const result = validate(UUIDSchema, { id })
+  const { id, userId } = req.params
+  const result = (id) ? validate(UUIDSchema, { id }) : (userId) ? validate(UUIDSchema, { id: userId }) : { error: 'An UUID is required' }
   if (!result.success) return res.send(result.error)
   next()
 }
