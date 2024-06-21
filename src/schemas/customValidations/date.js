00d6370ffd9d82faababf64950d6421d958c67a8
devEddu17x <eduarjma@gmail.com>
2024-06-20 20:40:36 -0500

    fix: bad comparison in isAfter1924 function

--- src/schemas/customValidations/date.js
+++ src/schemas/customValidations/date.js
@@ -1,1 +1,1 @@
-export const isAfter1924 = (date) => new Date(date) > new Date('2020-01-01T00:00:00.000Z')
+export const isAfter1924 = (date) => new Date(date) > new Date('1924-01-01T00:00:00.000Z')
