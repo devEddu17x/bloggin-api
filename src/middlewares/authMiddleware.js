4cbba6b050aefe53c0008aa4b79cbb1e6b7bb092
devEddu17x <eduarjma@gmail.com>
2024-06-24 15:38:45 -0500

    feat: implement auth middleware

---
+++ src/middlewares/authMiddleware.js
@@ -0,0 +1,8 @@
+import jwt from 'jsonwebtoken'
+export const authMiddleware = (req, res, next) => {
+  const token = req.cookies.access_token
+  const tokenData = jwt.decode(token)
+  if (!tokenData.userId || !tokenData.username || !tokenData.name || !tokenData.lastName) return res.send({ error: 'Unexpected error ocurred' })
+  req.data = tokenData
+  next()
+}
