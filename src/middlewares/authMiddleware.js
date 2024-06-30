c531d8689068fea95d855062cba7cb81a2144f57
devEddu17x <eduarjma@gmail.com>
2024-06-30 16:48:39 -0500

    fix: auth middleware

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,7 +1,9 @@
 import jwt from 'jsonwebtoken'
 export const authMiddleware = (req, res, next) => {
   const token = req.cookies.access_token
+  if (!token) return res.status(401).send({ error: 'You must be logged in our app' })
   const tokenData = jwt.decode(token)
+  console.log(tokenData)
   if (!tokenData.userId || !tokenData.username || !tokenData.name || !tokenData.lastName) return res.status(401).send({ error: 'Unexpected error ocurred' })
   req.data = tokenData
   next()
