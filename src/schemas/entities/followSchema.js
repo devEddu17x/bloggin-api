1c1639bd5a3a7c25ba57ce7cd69af4a72a0d3e50
devEddu17x <eduarjma@gmail.com>
2024-07-02 20:58:17 -0500

    refactor: follow schema

--- src/schemas/entities/followSchema.js
+++ src/schemas/entities/followSchema.js
@@ -1,7 +1,7 @@
 import { z } from 'zod'
 import { ERRORS } from '../messages/error.js'
 export const followsSchema = z.object({
-  followerID: z.string(ERRORS.REQUIRED('User ID'))
+  followerId: z.string(ERRORS.REQUIRED('User ID'))
     .uuid(ERRORS.INVALID('UUID')),
 
   followedId: z.string(ERRORS.REQUIRED('User ID'))
